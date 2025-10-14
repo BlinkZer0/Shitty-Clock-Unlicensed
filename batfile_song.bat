2371f79c22cb030b502d76542b0f4ab709a44a77
BlinkZer0 <randytmork@gmail.com>
2025-10-13 22:14:05 -0500

    Create batfile_song.bat

---
+++ batfile_song.bat
@@ -0,0 +1,56 @@
+@echo off
+color 0A
+cls
+
+timeout /t 1 /nobreak >nul
+echo.
+echo Bat file, bat file, whatcha gonna do?
+timeout /t 2 /nobreak >nul
+echo Whatcha gonna do when I run you?
+timeout /t 2 /nobreak >nul
+echo.
+echo Bat file, bat file, whatcha gonna do?
+timeout /t 2 /nobreak >nul
+echo Whatcha gonna do when I click you through?
+timeout /t 3 /nobreak >nul
+echo.
+echo.
+echo === Verse 1 ===
+timeout /t 1 /nobreak >nul
+echo Scripts compiled in the dead of night
+timeout /t 2 /nobreak >nul
+echo Automating tasks left and right
+timeout /t 2 /nobreak >nul
+echo When your mouse gets tired and your fingers ache
+timeout /t 2 /nobreak >nul
+echo That's when batch commands are awake!
+timeout /t 3 /nobreak >nul
+echo.
+echo.
+echo === Chorus ===
+timeout /t 1 /nobreak >nul
+echo Bat file, bat file, whatcha gonna do?
+timeout /t 2 /nobreak >nul
+echo Whatcha gonna do when I run you?
+timeout /t 2 /nobreak >nul
+echo Echo statements printing on my screen
+timeout /t 2 /nobreak >nul
+echo Cleanest automation you've ever seen!
+timeout /t 3 /nobreak >nul
+echo.
+echo.
+echo === Verse 2 ===
+timeout /t 1 /nobreak >nul
+echo Nobody nests loops quite like you do
+timeout /t 2 /nobreak >nul
+echo GOTO statements jumping right on through
+timeout /t 2 /nobreak >nul
+echo When the system needs a helping hand
+timeout /t 2 /nobreak >nul
+echo You execute just like we planned!
+timeout /t 3 /nobreak >nul
+echo.
+echo.
+echo 🦇 [END] 🦇
+timeout /t 2 /nobreak >nul
+pause
