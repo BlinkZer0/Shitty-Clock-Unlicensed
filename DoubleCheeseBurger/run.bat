f8b85e3b7d79cd1bfbd24b3ca78ca33fc0fa24d8
BlinkZer0 <randytmork@gmail.com>
2025-10-14 18:30:57 -0500

    DoubleCheeseBurger demos

---
+++ DoubleCheeseBurger/run.bat
@@ -0,0 +1,8 @@
+@echo off
+echo Running DoubleCheeseBurger Examples...
+echo.
+python examples\simple_demo.py
+echo.
+echo.
+python examples\raycaster.py
+pause
---
+++ DoubleCheeseBurger/run.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "Running DoubleCheeseBurger Examples..."
+echo
+python3 examples/simple_demo.py
+echo
+echo
+python3 examples/raycaster.py
+read -p "Press Enter to continue..."
